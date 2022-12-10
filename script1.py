4972d089b2f8ca11fed92839a9e438478b061672
RepenkoD <120255219+RepenkoD@users.noreply.github.com>
2022-12-10 16:03:41 +0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -3,6 +3,13 @@ import streamlit as st
 import torchaudio
 from speechbrain.pretrained import EncoderClassifier
 
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+stt_button = Button(label="Speak", width=100)
+
+
 
 def load_audio():
     """Создание формы для загрузки аудио"""
@@ -33,6 +40,48 @@ audio_data = load_audio()
 
 result = st.button('Распознать аудиофайл')
 #Если кнопка нажата, то запускаем распознавание
+
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
+
+
+
+
+
+
+
+
+
+
 
 if result:
     signal = language_id.load_audio("temp.wav")
